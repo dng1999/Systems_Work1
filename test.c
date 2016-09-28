b4736e94d28f599245ef8b0021e9cb586ec928bc
yureistar <dng1@stuy.edu>
2016-09-28 11:46:41 -0400

    work

--- test.c
+++ test.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char strLen(char *s){
+int myLen(char *s){
   int i = 0;
   while (s[i]){
     i++;
@@ -9,17 +10,17 @@ char strLen(char *s){
   return i;
 }
 
-char *strnCpy(char *dest, char *src, int n){
+char *mynCpy(char *dest, char *src, int n){
   int i = 0;
-  while (src[i] || i < n){
+  while (src[i] && i < n){
     dest[i] = src[i];
     i++;
   }
   return src;
 }
 
-char *strCat(char *dest, char *src){
-  int i = strLen(dest);
+char *myCat(char *dest, char *src){
+  int i = myLen(dest);
   while (*src){
     dest[i] = *src;
     src++;
@@ -28,7 +29,7 @@ char *strCat(char *dest, char *src){
   return dest;
 }
 
-char strCmp(char *s1, char *s2){
+int myCmp(char *s1, char *s2){
   while (*s1 && (*s1 == *s2)){
     s1++;
     s2++;
@@ -36,39 +37,62 @@ char strCmp(char *s1, char *s2){
   return *s1-*s2;
 }
 
-char strChr(char *str, char c){
-  char i = 0;
-  while (*str){
-    if (*str == c) return i;
-    str++;
-    i++;
+char *myChr(char *s, char c){
+  while (*s){
+    if (*s == c) return s;
+    s++;
   }
   return NULL;
 }
 
 int main(){
-  char s1[] = "Hello";
-  char s2[20];
-  
+  char Ys1[] = "Hello";
+  char Ys2[20] = "";
+
+  char Cs1[] = "Hello";
+  char Cs2[20] = "";
+  /////////////////////////////////////////////////////////////////////
+
   printf("Before applying functions:\n");
-  printf("s1 = %s, length = %d\n", s1, strLen(s1));
-  printf("s2 = %s, length = %d\n", s2, strLen(s2));
+  printf("\tY|Ys1 = %s, length = %d\n", Ys1, myLen(Ys1));
+  printf("\tC|Cs1 = %s, length = %zu\n\n", Cs1, strlen(Cs1));
+  
+  printf("\tY|Ys2 = %s, length = %d\n", Ys2, myLen(Ys2));
+  printf("\tC|Cs2 = %s, length = %zu\n\n", Cs2, strlen(Cs2));
   
   printf("\nTesting regular functions:\n");
-  strnCpy(s2,s1,3);
-  printf("Copy s1 to s2 (first three): s2 = %s, length = %d\n", s2, strLen(s2));
-  strCat(s2, "Man");
-  printf("Cat 'Man' to s2: s2 = %s, length = %d\n", s2, strLen(s2));
-  printf("s1 = %s, length = %d\n", s1, strLen(s1));
+  mynCpy(Ys2,Ys1,3);
+  printf("\tmynCpy(Ys2,Ys1,3): Ys2 = %s, length = %d\n", Ys2, myLen(Ys2));
+  strncpy(Cs2,Cs1,3);
+  printf("\tstrncpy(Cs2,Cs1,3): Cs2 = %s, length = %zu\n\n", Cs2, strlen(Cs2));
 
-  printf("\nTesting strcmp:\n");
-  printf("compare ab to abc: %d\n", strCmp("ab","abc"));
-  printf("compare abc to ab: %d\n", strCmp("abc","ab"));
-  printf("compare abc to abc: %d\n", strCmp("abc","abc"));
+  myCat(Ys2, "Man");
+  printf("\tmyCat(Ys2, 'Man): Ys2 = %s, length = %d\n", Ys2, myLen(Ys2));
+  strcat(Cs2, "Man");
+  printf("\tstrcat(Cs2, 'Man): Cs2 = %s, length = %zu\n\n", Cs2, strlen(Cs2));
+  
+  printf("\tYs1 = %s, length = %d\n", Ys1, myLen(Ys1));
+  printf("\tCs1 = %s, length = %zu\n\n", Cs1, strlen(Cs1));
 
-  printf("\nTesting strchr:\n");
-  printf("Looking for 1 in Hello123: [%d]\n", strChr("Hello123",'1'));
-  printf("Looking for 0 in Hello123: [%d]\n", strChr("Hello123",'0'));
+  printf("\nTesting mycmp:\n");
+  printf("\tmyCmp('ab','abc'): %d\n", myCmp("ab","abc"));
+  printf("\tstrcmp('ab','abc'): %d\n\n", strcmp("ab","abc"));
+
+  printf("\tmyCmp('abc','ab'): %d\n", myCmp("abc","ab"));
+  printf("\tstrcmp('abc','ab'): %d\n\n", strcmp("abc","ab"));
+  
+  printf("\tmyCmp('abc','abc'): %d\n", myCmp("abc","abc"));
+  printf("\tstrcmp('abc','abc'): %d\n\n", strcmp("abc","abc"));
+  
+  printf("\tmyCmp('abc','abd'): %d\n", myCmp("abc","abd"));
+  printf("\tstrcmp('abc','abd'): %d\n\n", strcmp("abc","abd"));
+  
+  printf("\nTesting mychr:\n");
+  printf("\tmyChr('Hello123','1'): [%p]\n", myChr("Hello123",'1'));
+  printf("\tstrchr('Hello123','1'): [%p]\n\n", strchr("Hello123",'1'));
+
+  printf("\tmyChr('Hello123','0'): [%p]\n", myChr("Hello123",'0'));
+  printf("\tstrchr('Hello123','0'): [%p]\n\n", myChr("Hello123",'0'));
 
   return 0;
 }
